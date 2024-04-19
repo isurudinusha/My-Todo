02b1d383a9e7df7bf38528e478f039c7df9629ed
Isuru Dinusha <isurudinusha@gmail.com>
2024-04-19 03:05:41 +0100

    baseURL

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // Creating an instance of axios with custom configuration
 const instance = axios.create({
     // Setting the base URL for the HTTP requests
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://my-todo-yc2c.onrender.com'
     // For local development
     // http://localhost:5000
     // When deploying, use the following URL
