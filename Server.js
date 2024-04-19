f4396efbdbe318bf5570efdcf70b22edfadb75ec
Isuru Dinusha <isurudinusha@gmail.com>
2024-04-19 02:22:19 +0100

    css update

--- Server.js
+++ Server.js
@@ -1,32 +1,30 @@
+// Importing necessary modules
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+// Importing routes for the Todo application
 const routes = require('./routes/TodoRoutes');
 
+// Loading environment variables from .env file
 require('dotenv').config();
 
+// Initializing express application
 const app = express();
 
+// Setting up the port for the server
 const PORT = process.env.PORT || 5000;
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
+
+// Middleware to enable CORS (Cross Origin Resource Sharing)
 app.use(cors());
 
-
-
+// Connecting to MongoDB using mongoose
 mongoose.
     connect(process.env.MONGODB_URL)
     .then(() => {
+        // Log to console when successfully connected to MongoDB
         console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-        console.log(error);
     });
-
-
-app.use(routes);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -1,50 +1,70 @@
+// Importing the Todo model and mongoose
 const Todo = require('../models/TodoModel');
 const mongoose = require('mongoose');
 
+// Function to get all todos
 module.exports.getTodos = async (req, res) => {
     try {
+        // Fetching all todos from the database and sorting them by creation date
         const todos = await Todo.find().sort({ createdAt: -1 });
+        // Sending the todos as a response with status code 200
         res.status(200).json(todos);
     } catch (error) {
+        // Sending an error message with status code 404 if something goes wrong
         res.status(404).json({ message: error.message });
     }
 };
 
+// Function to create a new todo
 module.exports.createTodo = async (req, res) => {
     try {
+        // Getting the todo data from the request body
         const todo = req.body;
+        // Creating a new Todo instance with the provided data
         const newTodo = new Todo(todo);
+        // Saving the new todo to the database
         await newTodo.save();
+        // Sending the new todo as a response with status code 201
         res.status(201).send(newTodo);
     } catch (error) {
+        // Sending an error message with status code 500 if something goes wrong
         res.status(500).send({ message: error.message });
     }
 };
 
+// Function to update a todo
 module.exports.updateTodo = async (req, res) => {
-    const { id } = req.params;
+    const { id: todoID, ...update } = req.body;
     try {
-        if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No todo with that id');
-        const todoID = { _id: id };
-        const update = { completed: true };
+        // Updating the todo with the provided id and sending the updated todo as a response
         const updateTodo = await Todo.findByIdAndUpdate(todoID, update);
         res.status(200).send(updateTodo);
     } catch (error) {
+        // Sending an error message with status code 500 if something goes wrong
         res.status(500).send({ message: error.message });
     }
 };
 
+// Function to delete a todo
 module.exports.deleteTodo = async (req, res) => {
+    // Getting the id of the todo from the request parameters
     const { id } = req.params;
     try {
-        if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No todo with that id');
+        // Checking if the provided id is a valid MongoDB ObjectId
+        if (!mongoose.Types.ObjectId.isValid(id))
+            // If the id is not valid, send a 404 status code with a message
+            return res.status(404).send('No todo with that id');
+
+        // If the id is valid, find the todo by id and delete it
         await Todo.findByIdAndDelete(id);
+
+        // Send a 200 status code with a success message
         res.status(200).send('Todo deleted successfully');
     } catch (error) {
+        // Sending an error message with status code 500 if something goes wrong
         res.status(500).send({ message: error.message });
     }
 };
-
 
 
 
